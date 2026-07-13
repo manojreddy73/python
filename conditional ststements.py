cff590f0f2b54e499642edf29150d433bfbdedfd
manojreddy73 <manojreddyveeram@gmail.com>
2026-07-13 12:27:35 +0530

    Add files via upload

---
+++ conditional ststements.py
@@ -0,0 +1,64 @@
+# task 1
+'''a=int(input("account balance"))
+if ('account balance is suffcient'):
+    print("allow withdrawal")
+else:
+    print("in sufficient")'''
+# task 2
+'''withdraw_amount=int(input("jack"))
+avalable_amount=int(input("alan"))
+if avalable_amount>=withdraw_amount:
+    print("allow withdraw")
+else:
+    print("in sufficient")'''
+
+# task 3
+'''username=input()
+password=input()
+if username==password:
+    print(" succesful login")
+else:
+    print("invalid credintials")'''
+# task 4
+'''purchase_amount=int(input("amount"))
+if purchase_amount>5000:
+    print("20% discount")
+elif purchase_amount>2000 and purchase_amount<5000:
+    print("10% discount")
+else:
+    print("no discount")'''
+# task 5
+'''current_units=int(input("units"))
+if current_units<100:
+    print("two ruppes per unit")
+elif current_units<300:
+    print(" four rupees per unit")
+else:
+    print("six rupees per unit")'''
+
+# task 6
+'''age=int(input("Age"))
+salary=int(input("Salary"))
+credit_score=int(input("Credit"))
+if age>=21:
+    if salary>=30000:
+        if credit_score>=750:
+            print("bank loan is eligible")
+        else:
+            print("not eligible")
+    else:
+        print("salary is not equal")
+else:
+    print("age is not matched")'''
+
+# task 7
+'''bill_amount=int(input("amount"))
+membership=bool(input("ship"))
+if bill_amount>2000 and membership==True:
+    print("15 percent discount")
+else:
+    print("no discount")'''
+            
+        
+    
+    
