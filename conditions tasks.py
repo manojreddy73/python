2f0933cd5a062bc37a7a32f29a9a09c8167be842
manojreddy73 <manojreddyveeram@gmail.com>
2026-07-13 12:15:37 +0530

    Add files via upload

---
+++ conditions tasks.py
@@ -0,0 +1,94 @@
+#tasks:
+
+# task 1
+'''age=int(input())
+if age>=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")'''
+
+#task 2
+'''a=int(input())
+if a%2==0:
+    print("even")
+else:
+    print("odd")'''
+#task 3
+'''year=int(input())
+if year/4==0:
+    print("leap year")
+else:
+    print("not leap year")'''
+# task 4
+'''name=input()
+if name=="jack":
+    print("welcome",name)
+else:
+    print("welcome guest")'''
+# task 5
+'''names=["jack","alan","brook","henry","jos"]
+name=input().lower()
+if name in names:
+    print("welcome",name)
+else:
+    print("welcome guest")'''
+# task 6
+'''lst=['a','e','i','o','u']
+letter=input().lower()
+if letter in lst:
+    print("it is vowel")
+else:
+    print("it is consonent")'''
+
+# task 7
+'''username=input()
+password=input()
+if username=="jack" and password==1234:
+        print("login successful")
+else:
+     print("invalid credentials")'''      
+
+# task 8      
+'''age=int(input("enter the age"))
+marks=int(input("enter the marks"))
+attendance=int(input("enter the attendance"))
+if age>=18:
+    print("vote is eligible")
+if marks>80:
+    print("eligible for scholarship")
+if attendance>70:
+    print("not eligible")'''
+
+# TASK 9
+
+'''
+a=int(input("enter your price"))
+if a==1200:
+    print("Redveluet cake")
+elif a==1000:
+    print("Almond cake")
+elif a==800:
+    print("chocolate cake")
+elif a==600:
+    print("butterscoch cake")
+else:
+    print("cake is not avaliable")'''
+
+# TASK 10
+
+'''a=str(input())
+if a=="BBQ pizza":
+    print("1000")
+elif a=="crispy chicken pizza":
+    print("800")
+elif a=="panner pizza":
+    print("600")
+elif a=="corn pizza":
+    print("400")
+elif a=="coke":
+    print("200")
+else:
+    print("item is not avaliable")'''
+
+
+    
