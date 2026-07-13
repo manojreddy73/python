a964b0a18cd9d26822bf1482c7dc039992c5c08c
manojreddy73 <manojreddyveeram@gmail.com>
2026-07-13 12:22:16 +0530

    Add files via upload

---
+++ for loop tasks.py
@@ -0,0 +1,65 @@
+
+# task 1
+'''lst=[200,400,600,800,1000]
+total_bill=0
+for i in lst:
+    total_bill=total_bill+i
+print("total price:",total_bill) '''   
+
+ # task 2   
+'''lst=[25,50,34,45,60,24,35]
+for temp in lst:
+    if temp>35:
+        print(temp)'''
+# task 3
+'''salary=[30000,40000,50000,50005,65000,77000]
+for sal in salary:
+    if sal>50000:
+        print(sal)'''
+
+# task 4  
+'''price=[2000,3000,4000,5000,6000]
+for i in price:
+    discount_price = i * 0.90
+    print(discount_price)'''    
+# task 5
+'''marks=[39,40,67,99,100,22,25]
+for i in marks:
+    if i>35:
+        print(i)'''
+# task 6
+'''for i in range(2,19,2):
+    print(i)'''
+
+# task 7
+'''for i in range(5,46,5):
+    print(i)'''
+# task 8
+'''for i in range(0,28,3):
+    print(i,end=" ")'''
+
+# task 9
+'''while True:
+    marks=int(input("marks"))
+    if marks in range(91,101):
+        print("grade-A")
+    elif marks in range(81,91):
+        print("grade-B")
+    elif marks in range(71,81):
+        print("grade-C")
+    elif marks in range(50,71):
+        print("grade_D")
+    else:
+        print("fail")'''   
+ # task 10           
+'''bank_transactions=[10000,12000,34000,77000,99000]
+for i in bank_transactions:
+    if i>10000:
+        print(i)'''
+
+
+
+      
+        
+        
+
